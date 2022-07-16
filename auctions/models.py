2ee989ac7ea6efb1879e51d1e079801b8aabc96e
Minh-Quan Phan <minh-quan@GRAY.local>
2022-07-16 15:47:20 +0700

    update

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,34 @@
+from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    url = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now_add=True)
+    starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.category}"
+
+
+class Bid(models.Model):
+    pass
+
+
+class Comment(models.Model):
     pass
